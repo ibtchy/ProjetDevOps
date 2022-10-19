98c14f2f2b0c453765c63f28bf89016e89989602
ibtihel <ibtihel.awledsoula@esprit.tn>
2022-10-19 14:02:57 +0100

    first comit projet

---
+++ src/main/java/tn/esprit/spring/ProjectAchatApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAchatApplication.class, args);
+	}
+
+}
