a4d4d2c16432087ea6ba573a2578d90a8034564c
salma <salma.mouelhi@esprit.tn>
2022-10-23 21:08:07 +0100

    class

--- src/main/java/entities/Stock.java
+++ src/main/java/entities/Stock.java
@@ -1,5 +1,42 @@
 package entities;
 
-public class Stock {
+import java.io.Serializable;
+import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Stock implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idStock;
+	private String libelleStock;
+	private Integer qte;
+	private Integer qteMin;
+	@OneToMany(mappedBy = "stock")
+	@JsonIgnore
+	private Set<Produit> produits;
+	public Stock(String libelleStock, Integer qte, Integer qteMin) {
+		super();
+		this.libelleStock = libelleStock;
+		this.qte = qte;
+		this.qteMin = qteMin;
+	}
 }
