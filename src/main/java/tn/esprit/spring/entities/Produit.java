5c76bd5b0d1c7ad52628f1db006d5936a41db4c3
oussamabouguerra <47630354+oussamabouguerra@users.noreply.github.com>
2022-11-01 11:52:50 +0100

    first commit

--- src/main/java/tn/esprit/spring/entities/Produit.java
+++ src/main/java/tn/esprit/spring/entities/Produit.java
@@ -25,9 +25,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Produit implements Serializable {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
