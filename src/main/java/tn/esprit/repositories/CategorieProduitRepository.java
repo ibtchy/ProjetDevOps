517588bdcacf64f613f3b0b39b3861562f2b92c3
ibtihel <ibtihel.awledsoula@esprit.tn>
2022-11-01 11:39:42 +0100

    commit p

--- src/main/java/tn/esprit/repositories/CategorieProduitRepository.java
+++ src/main/java/tn/esprit/repositories/CategorieProduitRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import tn.esprit.spring.entities.CategorieProduit;
 
-
 @Repository
 public interface CategorieProduitRepository extends JpaRepository<CategorieProduit, Long>{
 
