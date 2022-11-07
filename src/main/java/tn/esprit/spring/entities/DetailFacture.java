26c5013dbb4797d6257b71af747db6098a1b7158
oussamabouguerra <47630354+oussamabouguerra@users.noreply.github.com>
2022-11-07 21:14:46 +0100

    aavvv

--- src/main/java/tn/esprit/spring/entities/DetailFacture.java
+++ src/main/java/tn/esprit/spring/entities/DetailFacture.java
@@ -1,14 +1,11 @@
 package tn.esprit.spring.entities;
 
 import java.io.Serializable;
-import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.ManyToOne;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,19 +17,19 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class DetailFacture implements Serializable{
+public class DetailFacture implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idDetailFournisseur;
-	private String email;
-	@Temporal(TemporalType.DATE)
-	private Date dateDebutCollaboration;
-	private String adresse;
-	private String matricule;
-	@OneToOne(mappedBy="detailFournisseur")
+	private Long idDetailFacture;
+	private Integer qteCommandee;
+	private float prixTotalDetail;
+	private Integer pourcentageRemise;
+	private float montantRemise;
+	@ManyToOne
+	private Produit produit;
+	@ManyToOne
 	@JsonIgnore
-	private Fournisseur fournisseur;
-	
-}
+	Facture facture;
 
+}
