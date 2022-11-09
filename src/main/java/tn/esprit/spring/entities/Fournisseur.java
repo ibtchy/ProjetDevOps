ff23cf8f7379cce8d00e3a07d1c7c402746c53ef
med cherif lakhoua <medcherif.lakhoua@esprit.tn>
2022-11-09 01:56:47 +0100

    sonar 2

--- src/main/java/tn/esprit/spring/entities/Fournisseur.java
+++ src/main/java/tn/esprit/spring/entities/Fournisseur.java
@@ -44,6 +44,24 @@ public class Fournisseur implements Serializable {
     private Set<SecteurActivite> secteurActivites;
     @OneToOne(cascade= CascadeType.ALL,fetch=FetchType.EAGER)
     private DetailFournisseur detailFournisseur;
+	public Fournisseur(Long idFournisseur, String code, String libelle) {
+		super();
+		this.idFournisseur = idFournisseur;
+		this.code = code;
+		this.libelle = libelle;
+	
+	}
+	
+	public Fournisseur(long idFournisseur, String code, String libelle) {
+		this.idFournisseur = idFournisseur;
+		this.code = code;
+		this.libelle = libelle;
+		this.categorieFournisseur = ordinaire;
+	
+
+
+
+	}
     
 
 	
