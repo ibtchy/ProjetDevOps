92f4c060b992b25e775610753520a0bddf4cff17
salma <salma.mouelhi@esprit.tn>
2022-11-13 12:49:31 +0100

    aa

--- src/test/java/tn/esprit/spring/services/StockServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/StockServiceImplTest.java
@@ -1,33 +1,28 @@
 package tn.esprit.spring.services;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.Assert.assertEquals;
-//import org.junit.Test;
+
 import org.junit.Test;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-
-
-import tn.esprit.spring.entities.Facture;
 import tn.esprit.spring.entities.Stock;
-import tn.esprit.spring.repositories.FactureRepository;
+
 import tn.esprit.spring.repositories.StockRepository;
 
 @SpringBootTest
