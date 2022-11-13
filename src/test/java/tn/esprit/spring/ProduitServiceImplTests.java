9981dbd9bcb0a60aa461f55cae1a59b1ef7b70b9
ibtihel <ibtihel.awledsoula@esprit.tn>
2022-11-13 13:20:18 +0100

    yt

--- src/test/java/tn/esprit/spring/ProduitServiceImplTests.java
+++ src/test/java/tn/esprit/spring/ProduitServiceImplTests.java
@@ -1,7 +1,5 @@
 package tn.esprit.spring;
 
-import static org.junit.Assert.assertEquals;
-
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -17,36 +15,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.test.context.junit4.SpringRunner;
-
-
 import lombok.extern.slf4j.Slf4j;
 import tn.esprit.repositories.ProduitRepository;
 import tn.esprit.spring.entities.Produit;
 import tn.esprit.spring.services.ProduitServiceImpl;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.Test;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.runner.RunWith;
-
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-
 import org.mockito.junit.MockitoJUnitRunner;
+
+
+
 //@ExtendWith(MockitoExtension.class)
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest //(classes=ProduitServiceImpl.class)
